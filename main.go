27ab4dcfafd864dc215e3787e27331db698b7977
cienfuegos-dev <cienfuegos.dev@gmail.com>
2022-09-11 11:55:24 -0500

    Refactor print code

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math/rand"
 	"os/exec"
 	"sync"
-	"time"
 )
+import p "rex-daemon/rexprint"
 
 /*
 Restart policies
@@ -16,7 +15,7 @@ k8s: Always, OnFailure, Never
 
 func main() {
 	var wg sync.WaitGroup
-	colors := getRandomColors()
+	colors := p.GetRandomColors()
 
 	commands := [][]string{
 		{"./demo-exes/03-dynamic-sleep-cpp.exe", "2", "2", "2", "1", "1"},
@@ -34,65 +33,6 @@ func main() {
 	wg.Wait()
 }
 
-func printAllColors() {
-	colors := getRandomColors()
-
-	for i, c := range colors {
-		fmt.Println(fmt.Sprintf("\x1b[%dm%s\x1b[0m", c, fmt.Sprintf("[%d]:%d", i, c)))
-	}
-}
-
-func getRandomColors() []int {
-	colors := []int{
-		31,
-		32,
-		33,
-		34,
-		35,
-		36,
-		//37, Barely visible
-		//90, Barely visible
-		//91, Very similar to red (31)
-		92,
-		93,
-		94,
-		95,
-		96,
-		97,
-	}
-
-	// Shuffle colors array
-	rand.Seed(time.Now().UnixNano())
-	for i := range colors {
-		j := rand.Intn(i + 1)
-		colors[i], colors[j] = colors[j], colors[i]
-	}
-	return colors
-}
-
-func printLnColor(id string, colors []int, i int, msg ...any) {
-	colorIndex := i % len(colors)
-	colored := fmt.Sprintf("\x1b[%dm%s\x1b[0m", colors[colorIndex], fmt.Sprintf("[%s]", id))
-	msg = append([]any{colored}, msg...)
-	fmt.Println(msg...)
-}
-
-func getColored(color int, text string) string {
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color, text)
-}
-
-func dim(text string) string {
-	return getColored(37, text)
-}
-
-func errColor(text string) string {
-	return getColored(41, text)
-}
-
-func outColor(text string) string {
-	return getColored(42, text)
-}
-
 func runCommand(i int, group *sync.WaitGroup, colors []int, command string, args ...string) {
 	// Sync with wait group
 	defer group.Done()
@@ -106,21 +46,21 @@ func runCommand(i int, group *sync.WaitGroup, colors []int, command string, args
 
 	// Start command
 	if err = cmd.Start(); err != nil {
-		printLnColor(fmt.Sprintf("%d", i), colors, i, err.Error())
+		p.PrintLnColor(fmt.Sprintf("%d", i), colors, i, err.Error())
 	}
 
 	// ID format: index:PID:attempt where attempt increases by one each time the command is restarted
 	id := fmt.Sprintf("%d:%d:0", i, cmd.Process.Pid)
 
 	// TODO: Beware of printing all args, since the user might pass sensitive data as env vars for the game.
-	printLnColor(id, colors, i, dim(fmt.Sprintf("running '%s' with args %s PID %d", command, args, cmd.Process.Pid)))
+	p.PrintLnColor(id, colors, i, p.Dim(fmt.Sprintf("running '%s' with args %s PID %d", command, args, cmd.Process.Pid)))
 
 	// Print realtime stdout from command
 	go func() {
 		scanner := bufio.NewScanner(stdout)
 		for scanner.Scan() {
 			m := scanner.Text()
-			printLnColor(id, colors, i, outColor("STDOUT"), m)
+			p.PrintLnColor(id, colors, i, p.OutColor("STDOUT"), m)
 		}
 	}()
 
@@ -129,14 +69,14 @@ func runCommand(i int, group *sync.WaitGroup, colors []int, command string, args
 		scannerErr := bufio.NewScanner(stderr)
 		for scannerErr.Scan() {
 			m := scannerErr.Text()
-			printLnColor(id, colors, i, errColor("STDERR"), m)
+			p.PrintLnColor(id, colors, i, p.ErrColor("STDERR"), m)
 		}
 	}()
 
 	// Wait for command to complete
 	if err := cmd.Wait(); err != nil {
-		printLnColor(id, colors, i, dim("terminated with error"), err.Error())
+		p.PrintLnColor(id, colors, i, p.Dim("terminated with error"), err.Error())
 	} else {
-		printLnColor(id, colors, i, dim("terminated"))
+		p.PrintLnColor(id, colors, i, p.Dim("terminated"))
 	}
 }
---
+++ rexprint/rexprint.go
@@ -0,0 +1,66 @@
+package rexprint
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func printAllColors() {
+	colors := GetRandomColors()
+
+	for i, c := range colors {
+		fmt.Println(fmt.Sprintf("\x1b[%dm%s\x1b[0m", c, fmt.Sprintf("[%d]:%d", i, c)))
+	}
+}
+
+func GetRandomColors() []int {
+	colors := []int{
+		31,
+		32,
+		33,
+		34,
+		35,
+		36,
+		//37, Barely visible
+		//90, Barely visible
+		//91, Very similar to red (31)
+		92,
+		93,
+		94,
+		95,
+		96,
+		97,
+	}
+
+	// Shuffle colors array
+	rand.Seed(time.Now().UnixNano())
+	for i := range colors {
+		j := rand.Intn(i + 1)
+		colors[i], colors[j] = colors[j], colors[i]
+	}
+	return colors
+}
+
+func PrintLnColor(id string, colors []int, i int, msg ...any) {
+	colorIndex := i % len(colors)
+	colored := fmt.Sprintf("\x1b[%dm%s\x1b[0m", colors[colorIndex], fmt.Sprintf("[%s]", id))
+	msg = append([]any{colored}, msg...)
+	fmt.Println(msg...)
+}
+
+func getColored(color int, text string) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color, text)
+}
+
+func Dim(text string) string {
+	return getColored(37, text)
+}
+
+func ErrColor(text string) string {
+	return getColored(41, text)
+}
+
+func OutColor(text string) string {
+	return getColored(42, text)
+}
