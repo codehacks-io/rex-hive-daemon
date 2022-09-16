8ca488d94a259bc8fb2cb3a5923fc92da60831e5
cienfuegos-dev <cienfuegos.dev@gmail.com>
2022-09-16 15:57:59 -0500

    Add machine meta package

---
+++ machine_meta/machine_meta.go
@@ -0,0 +1,83 @@
+package machine_meta
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func GetMachineMeta() MachineMeta {
+	m := MachineMeta{}
+
+	hostname, _ := os.Hostname()
+	m.Hostname = hostname
+	m.Goos = runtime.GOOS
+	m.Goarch = runtime.GOARCH
+
+	// Get LSB (Linux Standard Base) and Distribution information.
+	m.LsbRelease = LsbRelease{}
+	if r, err := exec.Command("lsb_release", "-i").Output(); err == nil {
+		m.LsbRelease.Id = string(r)
+	}
+	if r, err := exec.Command("lsb_release", "--description").Output(); err == nil {
+		m.LsbRelease.Description = string(r)
+	}
+	if r, err := exec.Command("lsb_release", "--release").Output(); err == nil {
+		m.LsbRelease.Release = string(r)
+	}
+	if r, err := exec.Command("lsb_release", "--codename").Output(); err == nil {
+		m.LsbRelease.Codename = string(r)
+	}
+
+	m.Uname = Uname{}
+	if r, err := exec.Command("uname", "--kernel-name").Output(); err == nil {
+		m.Uname.KernelName = string(r)
+	}
+	if r, err := exec.Command("uname", "--nodename").Output(); err == nil {
+		m.Uname.NodeName = string(r)
+	}
+	if r, err := exec.Command("uname", "--kernel-release").Output(); err == nil {
+		m.Uname.KernelRelease = string(r)
+	}
+	if r, err := exec.Command("uname", "--kernel-version").Output(); err == nil {
+		m.Uname.KernelVersion = string(r)
+	}
+	if r, err := exec.Command("uname", "--machine").Output(); err == nil {
+		m.Uname.Machine = string(r)
+	}
+	if r, err := exec.Command("uname", "--processor").Output(); err == nil {
+		m.Uname.Processor = string(r)
+	}
+	if r, err := exec.Command("uname", "--hardware-platform").Output(); err == nil {
+		m.Uname.HardwarePlatform = string(r)
+	}
+	if r, err := exec.Command("uname", "--operating-system").Output(); err == nil {
+		m.Uname.OperatingSystem = string(r)
+	}
+	return m
+}
+
+type LsbRelease struct {
+	Id          string // eg: Distributor ID: Ubuntu
+	Description string // eg: Description:    Ubuntu 20.04.5 LTS
+	Release     string // eg: Release:        20.04
+	Codename    string // eg: Codename:       focal
+}
+type Uname struct {
+	KernelName       string // eg: Linux
+	NodeName         string // eg: GODLIKE
+	KernelRelease    string // eg: 5.10.16.3-microsoft-standard-WSL2
+	KernelVersion    string // eg: #1 SMP Fri Apr 2 22:23:49 UTC 2021
+	Machine          string // eg: x86_64
+	Processor        string // eg: x86_64
+	HardwarePlatform string // eg: x86_64
+	OperatingSystem  string // eg: GNU/Linux
+}
+
+type MachineMeta struct {
+	Hostname   string
+	Goos       string
+	Goarch     string
+	LsbRelease LsbRelease
+	Uname      Uname
+}
