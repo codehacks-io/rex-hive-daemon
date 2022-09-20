5bcbc59c0135ff6d13acd212583b3cb40d063a64
cienfuegos-dev <cienfuegos.dev@gmail.com>
2022-09-20 14:52:41 -0500

    Remove demo mongo code

--- message_handler/message_handler.go
+++ message_handler/message_handler.go
@@ -2,12 +2,10 @@ package message_handler
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -222,44 +220,6 @@ func bulkStoreMessagesInMongo() {
 			fmt.Println(rexprint.Dim("Flush requested: There are pending messages to be stored in DB, will wait to flush..."))
 		}
 	}
-}
-
-func testMongo() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
-	uri := os.Getenv("MONGODB_URI")
-	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
-	}
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-	if err != nil {
-		fmt.Println(rexprint.ErrColor(err.Error()))
-	}
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			fmt.Println(rexprint.ErrColor(err.Error()))
-		}
-	}()
-
-	coll := client.Database("sample_mflix").Collection("movies")
-	title := "Back to the Future"
-	var result bson.M
-	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
-
-	if err == mongo.ErrNoDocuments {
-		fmt.Printf("No document was found with the title %s\n", title)
-		return
-	}
-
-	if err != nil {
-		fmt.Println(rexprint.ErrColor(err.Error()))
-	}
-	jsonData, err := json.MarshalIndent(result, "", "    ")
-	if err != nil {
-		fmt.Println(rexprint.ErrColor(err.Error()))
-	}
-	fmt.Printf("%s\n", jsonData)
 }
 
 func OnHiveMessage(message *hive_message.HiveMessage) {
