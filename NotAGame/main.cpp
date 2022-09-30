a6f4f235b03df2d5295e2b7bc6edd7422dae82e8
cienfuegos-dev <cienfuegos.dev@gmail.com>
2022-09-30 10:31:13 -0500

    Simplify cpp project with cmake

--- NotAGame/main.cpp
+++ NotAGame/main.cpp
@@ -6,27 +6,24 @@
 #include <thread>
 #include <string>
 
-int main(const int argc, const char *argv[])
-{
+int main(const int argc, const char *argv[]) {
     std::cout << "Hello from C++" << std::endl;
 
-    for (auto i = 1; i < argc; i++)
-    {
-        // Before sleep message
-        auto msg = std::format("{} of {}: will sleep for {} seconds", i, argc - 1, argv[i]);
-        const auto secondsToSleep = std::stoi(argv[i]);
+    const int total = argc - 1;
+    for (int i = 1; i < argc; i++) {
+        const int secondsToSleep = std::stoi(argv[i]);
 
-        if (secondsToSleep < 0)  std::cerr << msg << std::endl;
-        else std::cout << msg << std::endl;
+        // If seconds to sleep is negative, print to the stderr stream. Else, print to the stdout stream.
+        std::basic_ostream<char> *channel = secondsToSleep < 0 ? &std::cerr : &std::cout;
+
+        // Before sleep message
+        *channel << i << " of " << total << ": will sleep for" << secondsToSleep << std::endl;
 
         // Sleep
         std::this_thread::sleep_for(std::chrono::seconds(std::abs(secondsToSleep)));
 
         // After sleep message
-        msg = std::format("{} of {}: did sleep for {} seconds", i, argc - 1, argv[i]);
-
-        if (secondsToSleep < 0)  std::cerr << msg << std::endl;
-        else std::cout << msg << std::endl;
+        *channel << i << " of " << total << ": did sleep for" << secondsToSleep << std::endl;
     }
 
     std::cout << "Good bye from C++" << std::endl;
@@ -35,7 +32,7 @@ int main(const int argc, const char *argv[])
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
