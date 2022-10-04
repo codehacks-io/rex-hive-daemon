04ae25ac1f9190b02e9008e768108412127270c7
cienfuegos-dev <cienfuegos.dev@gmail.com>
2022-10-04 14:55:41 -0500

    Log message to detect cross-env-var contamination

--- NotAGameGolang/main.go
+++ NotAGameGolang/main.go
@@ -9,7 +9,8 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from golang")
+	signature := getEnvVarSignature()
+	fmt.Println(fmt.Sprintf("%sHello from golang", validateSignature(&signature)))
 	flag.Parse()
 	args := flag.Args()
 
@@ -27,7 +28,7 @@ func main() {
 		}
 
 		// Message before sleep
-		msgBefore := fmt.Sprintf("%d of %d: will sleep for %d seconds...", i+1, total, n)
+		msgBefore := fmt.Sprintf("%s%d of %d: will sleep for %d seconds...", validateSignature(&signature), i+1, total, n)
 		if n < 0 {
 			_, _ = fmt.Fprintln(os.Stderr, msgBefore)
 		} else {
@@ -37,7 +38,7 @@ func main() {
 		time.Sleep(time.Duration(secondsToSleep) * time.Second)
 
 		// Message after sleep
-		msgAfter := fmt.Sprintf("%d of %d: did sleep for %d seconds", i+1, total, n)
+		msgAfter := fmt.Sprintf("%s%d of %d: did sleep for %d seconds", validateSignature(&signature), i+1, total, n)
 		if n < 0 {
 			_, _ = fmt.Fprintln(os.Stderr, msgAfter)
 		} else {
@@ -45,5 +46,29 @@ func main() {
 		}
 	}
 
-	fmt.Println("Good bye from golang")
+	fmt.Println(fmt.Sprintf("%sGood bye from golang", validateSignature(&signature)))
+}
+
+func validateSignature(originalSignature *string) string {
+	updatedSignature := getEnvVarSignature()
+	if *originalSignature != updatedSignature {
+		printErrorLn("ERROR IN SIGNATURES: original ", *originalSignature, " does not mach updated one: ", updatedSignature)
+	}
+	sig := fmt.Sprintf("(%s===%s) ", *originalSignature, updatedSignature)
+	return sig
+}
+
+func getEnvVarSignature() string {
+	pubConn := os.Getenv("REX_PUBLIC_CONNECTIONS")
+	privConn := os.Getenv("REX_PRIVATE_CONNECTIONS")
+	if pubConn != privConn {
+		printErrorLn("CROSS ENV CONTAMINATION", pubConn, privConn)
+	}
+	return fmt.Sprintf("+%s,-%s", pubConn, privConn)
+}
+
+func printErrorLn(msg ...any) {
+	colored := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 41, "[error]")
+	msg = append([]any{colored}, msg...)
+	fmt.Println(msg...)
 }
